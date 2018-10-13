47e60b9439b79af568486cb1c7ec446f39ebd1fd
Janghoon Lee <janghoon@minerva.kgi.edu>
2018-10-13 04:42:35 -0700

    Add token sales handler

--- contracts/ShowerTokenSale.sol
+++ contracts/ShowerTokenSale.sol
@@ -6,10 +6,29 @@ contract ShowerTokenSale {
     address admin;
     ShowerToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor (ShowerToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender; // Assign an admin
         tokenContract = _tokenContract; // Assign Token Contract
         tokenPrice = _tokenPrice; // Assign Token Price
+    }
+
+    // Multiply
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    // Purchase Token
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
